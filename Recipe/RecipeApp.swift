9939271e379c9edcb9631d087d8872ebb4cd55a4
YAKUP YILDIRIM - Merkezi Mro Cozumleri Mudurlugu <YILDIRIMYAKUP@THY.COM>
2023-07-26 22:27:54 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by YAKUP YILDIRIM on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
